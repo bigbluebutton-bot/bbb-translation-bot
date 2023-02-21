29739075a2006e3bd184f6cc12ac148952797e03
stjukrop <julian.kropp@stud.h-da.de>
2023-02-21 12:39:16 +0100

    ✨ Client start multiple tcp connections

--- client.py
+++ client.py
@@ -1,14 +1,23 @@
 import socket
+import time
 
 
 # Connect to localhost:5000 and send a message
 def connect():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('localhost', 5000))
-        s.sendall(b'Hello, world')
-        s.close()
-    except:
-        print("Connection failed")
+    for i in range(10):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(('localhost', 5000))
+            try:
+                s.sendall(b'Hello, world' + str(i).encode())
+            except:
+                print("Send failed")
+
+            try:
+                s.close()
+            except:
+                print("Close failed")
+        except:
+            print("Connection failed")
 
 connect()
