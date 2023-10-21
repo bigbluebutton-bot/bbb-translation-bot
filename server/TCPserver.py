7c4ac2fd0928e394fab376e24de94535b49a5384
JulianKropp <JulianKropp@users.noreply.github.com>
2023-10-21 15:32:18 +0200

    🔥 🚚 Move EventHandler

--- server/TCPserver.py
+++ server/TCPserver.py
@@ -11,6 +11,8 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 import os
+import Event as event
+EventHandler = event.EventHandler
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -19,44 +21,6 @@ logging.basicConfig(level=logging.DEBUG)
 # Convert the byte string to a list of integers for logging
 def byte_string_to_int_list(byte_string):
     return [byte for byte in byte_string]
-
-class EventHandler:
-    """Class responsible for managing callbacks."""
-
-    def __init__(self):
-        logging.debug("Initializing EventHandler.")
-        self._callbacks = {}
-        self._event_lock = threading.Lock()
-
-    def add_event(self, callback):
-        """Add a new event callback and return its unique ID."""
-        with self._event_lock:
-            event_id = callback
-            logging.debug(f"Adding event with ID: {event_id}")
-            self._callbacks[event_id] = callback
-            return event_id
-
-    def remove_event(self, event_id):
-        """Remove an event callback using its ID."""
-        logging.debug(f"Removing event with ID: {event_id}")
-        with self._event_lock:
-            self._callbacks.pop(event_id, None)
-
-    def emit(self, *args):
-        """Trigger all the registered callbacks with the provided arguments."""
-        threads = []  # To keep track of the threads
-
-        with self._event_lock:
-            for event_id, callback in self._callbacks.items():
-                logging.debug(f"Emitting event with ID: {event_id}")
-                # Wrap the callback execution in a thread
-                t = threading.Thread(target=callback, args=args)
-                threads.append(t)
-                t.start()
-
-        # Wait for all threads to finish
-        for t in threads:
-            t.join()
 
 
 class Server:
--- server/UDPserver.py
+++ server/UDPserver.py
@@ -4,52 +4,13 @@ import threading
 import logging
 import inspect
 import time
+import Event as event
+EventHandler = event.EventHandler
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-
-
-class EventHandler:
-    """Class responsible for managing callbacks."""
-
-    def __init__(self):
-        logging.debug("Initializing EventHandler.")
-        self._callbacks = {}
-        self._event_lock = threading.Lock()
-
-    def add_event(self, callback):
-        """Add a new event callback and return its unique ID."""
-        with self._event_lock:
-            event_id = callback
-            logging.debug(f"Adding event with ID: {event_id}")
-            self._callbacks[event_id] = callback
-            return event_id
-
-    def remove_event(self, event_id):
-        """Remove an event callback using its ID."""
-        logging.debug(f"Removing event with ID: {event_id}")
-        with self._event_lock:
-            self._callbacks.pop(event_id, None)
-
-    def emit(self, *args):
-        """Trigger all the registered callbacks with the provided arguments."""
-        threads = []  # To keep track of the threads
-
-        with self._event_lock:
-            for event_id, callback in self._callbacks.items():
-                logging.debug(f"Emitting event with ID: {event_id}")
-                # Wrap the callback execution in a thread
-                t = threading.Thread(target=callback, args=args)
-                threads.append(t)
-                t.start()
-
-        # Wait for all threads to finish
-        for t in threads:
-            t.join()
 
 
 class Client:
