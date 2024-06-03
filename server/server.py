44315a235a7608aeef2dc664060c78c262e246b5
JulianKropp <JulianKropp@users.noreply.github.com>
2024-06-03 15:21:45 +0000

    ✨ Loading models multiple times

--- server/server.py
+++ server/server.py
@@ -48,19 +48,145 @@ processing_time = Histogram('speech_processing_time_seconds', 'Time taken to pro
 queue_wait_time = Histogram('client_queue_wait_time_seconds', 'Time a client has waited in the queue')
 total_processing_time = Histogram('total_processing_time_seconds', 'Time from receiving audio to sending transcription')
 
+# Vars
+client_dict = {}        # Dictionary with all connected clients
+client_queue = Queue()  # Queue with clients which have data to process
+client_dict_mutex = threading.Lock() # Mutex to lock the client_dict
+client_queue_mutex = threading.Lock() # Mutex to lock the client_queue
+
+# Load settings from env variables use default values if not set
+settings = load_settings()
+
+class worker:
+    def __init__(self, model_type = "tiny", english_only = "false", model_path = ""):
+        if english_only:
+            model_type = model_type + ".en"
+        self.model_type = model_type
+        self.model = None
+        self.running_mutex = threading.Lock()
+        self.model_path = model_path
+        self.running = False
+
+    def process(self):
+        if self.running_mutex.locked():
+            return # return if mutex is locked
+        
+        # Load model
+        with self.running_mutex:
+            if self.model_path != "":
+                logging.info(f"Loading model '{self.model_type}'...")
+                self.model = whisper.load_model(self.model_type, download_root=self.model_path)
+                logging.info("Model loaded")
+            else:
+                self.model = whisper.load_model(self.model_type)
+
+            # Start processing
+            self.running = True
+
+            time_to_sleep = 0.25
+            while self.running:
+                client = None
+                try:
+                    sleep(time_to_sleep)    # Infinite loops are bad for processors, must sleep.
+
+                    # If there is no data to process, sleep for a bit.
+                    if client_queue.empty():
+                        time_to_sleep = 0.25    # Set sleep time to 0.25 seconds if there is no data to process.
+                    else:
+                        time_to_sleep = 0.0     # Set sleep time to 0 seconds if there is data to process.
+
+                        # Process data from client
+                        processing_start_time = datetime.utcnow()
+                        client = None
+                        with client_queue_mutex:
+                            client = client_queue.get()
+                            queue_wait_time.observe((datetime.utcnow() - client.client_queue_wait_time).total_seconds()) # Observe the time the client has waited in the queue for prometheus
+                            time_data_received = client.time_data_received
+
+
+                        last_sample = None
+
+                        # Pull raw recorded audio from the queue.
+                        with client.mutex:
+                            if not client.data_queue.empty():
+                                if client.phrase_time is None:
+                                    client.phrase_time = processing_start_time
+
+                                # Concatenate our current audio data with the latest audio data.
+                                while not client.data_queue.empty():
+                                    data = client.data_queue.get()
+                                    client.last_sample += data
+
+                            last_sample = client.last_sample
+
+                            # set header
+                            if client.oggs_opus_header_frames_complete == False:
+                                id_header_frame, comment_header_frames = extract_ogg_header_frames(last_sample)
+                                if id_header_frame is not None and len(comment_header_frames) > 0:
+                                    client.oggs_opus_header_frames += id_header_frame.raw_data
+                                    for frame in comment_header_frames:
+                                        client.oggs_opus_header_frames += frame.raw_data
+                                    client.oggs_opus_header_frames_complete = True
+                                continue
+
+                        # # Write to file for debugging.
+                        # with open('/testing/sample.opus', 'wb') as f:
+                        #     f.write(last_sample)
+
+                        # Create temporary file for audio data which will be deleted after use.
+                        with tempfile.NamedTemporaryFile(prefix='tmp_audio_', suffix='.wav', dir=settings['RAM_DISK_PATH'], delete=True) as temp_file:
+                            # Convert opus to wav
+                            opus_data = io.BytesIO(last_sample)
+                            opus_audio = AudioSegment.from_file(opus_data, format="ogg", frame_rate=48000, channels=2, sample_width=2)
+                            opus_audio.export(temp_file.name, format="wav")
+                            
+                            # Transcribe audio data
+                            result = self.model.transcribe(temp_file.name, fp16=torch.cuda.is_available(), task=settings["TASK"])
+                            text = result['text'].strip()
+                            # logging.info(str.encode(text))
+
+                        # Set transcription
+                        with client.mutex:
+                            client.transcription = text
+
+                            # Send text to client
+                            try:
+                                client.send(str.encode(text))
+                            except:
+                                pass
+
+
+                        # If enough time has passed between recordings, consider the phrase complete.
+                        # Clear the current working audio buffer to start over with the new data.
+                        if client.phrase_time and processing_start_time - client.phrase_time > timedelta(seconds=settings["RECORD_TIMEOUT"]):
+                            # logging.info("Clear audio buffer")
+                            client.clear_buffer()
+
+                        processing_time.observe((datetime.utcnow() - processing_start_time).total_seconds())  # Observe processing time for prometheus
+                        total_processing_time.observe((datetime.utcnow() - time_data_received).total_seconds())  # Observe total processing time for prometheus
+                except Exception as e:
+                    logging.error(e)
+                    if client:
+                        client.stop()
+                        break
+        self.stop()
+
+    def stop(self):
+        if self.running_mutex.locked():
+            self.running = False
+            with self.running_mutex:
+                self.model = None
+
+
+    
+
+
 
 
 # Main function which handles all clients one by one (Queue).
 def main():
     global STATUS
     STATUS = "starting"
-
-
-    # Load settings from env variables or config file or use default values if not set
-    settings = load_settings()
-    # Print the settings for demonstration purposes
-    for key, value in settings.items():
-        logging.debug(f"{key}: {value}")
 
     # Start the health http-server (flask) in a new thread.
     webserverthread = threading.Thread(target=app.run, kwargs={'debug': False, 'host': settings["HOST"], 'port': settings["HEALTH_CHECK_PORT"]})
@@ -76,16 +202,6 @@ def main():
     model = settings["MODEL"]
     if settings["MODEL"] != "large" and settings["ONLY_ENGLISH"]:
         model = model + ".en"
-    logging.info(f"Loading model '{model}'...")
-    audio_model = whisper.load_model(model, download_root=settings["MODEL_PATH"])
-    logging.info("Model loaded")
-
-
-    # Vars
-    client_dict = {}        # Dictionary with all connected clients
-    client_queue = Queue()  # Queue with clients which have data to process
-    client_dict_mutex = threading.Lock() # Mutex to lock the client_dict
-    client_queue_mutex = threading.Lock() # Mutex to lock the client_queue
 
     # Create server
     srv = Server(settings["HOST"], settings["TCPPORT"], settings["UDPPORT"], settings["SECRET_TOKEN"], 4096, 5, 10, 1024, settings["EXTERNALHOST"])
@@ -145,105 +261,33 @@ def main():
     # Start server
     logging.info(f"Starting server: {settings['HOST']}:{settings['TCPPORT']}...")
     srv.start()
-    STATUS = "running"
     logging.info("Ready to transcribe. Press Ctrl+C to stop.")
 
 
-    # Main loop to process incomming data from all clients
-    time_to_sleep = 0.25    # Infinite loops are bad for processors, must sleep.
-    while True:
-        client = None
-        try:
-            sleep(time_to_sleep)    # Infinite loops are bad for processors, must sleep.
+    # create worker threaded
+    workers = []
+    for _ in range(2):
+        w = worker(model_type=settings["MODEL"], english_only=settings["ONLY_ENGLISH"], model_path=settings["MODEL_PATH"])
+        t = threading.Thread(target=w.process)
+        t.daemon = True
+        t.start()
+        workers.append(w)
 
-            # If there is no data to process, sleep for a bit.
-            if client_queue.empty():
-                time_to_sleep = 0.25    # Set sleep time to 0.25 seconds if there is no data to process.
-            else:
-                time_to_sleep = 0.0     # Set sleep time to 0 seconds if there is data to process.
+    STATUS = "running"
 
-                # Process data from client
-                processing_start_time = datetime.utcnow()
-                client = None
-                with client_queue_mutex:
-                    client = client_queue.get()
-                    queue_wait_time.observe((datetime.utcnow() - client.client_queue_wait_time).total_seconds()) # Observe the time the client has waited in the queue for prometheus
-                    time_data_received = client.time_data_received
-
-
-                last_sample = None
-
-                # Pull raw recorded audio from the queue.
-                with client.mutex:
-                    if not client.data_queue.empty():
-                        if client.phrase_time is None:
-                            client.phrase_time = processing_start_time
-
-                        # Concatenate our current audio data with the latest audio data.
-                        while not client.data_queue.empty():
-                            data = client.data_queue.get()
-                            client.last_sample += data
-
-                    last_sample = client.last_sample
-
-                    # set header
-                    if client.oggs_opus_header_frames_complete == False:
-                        id_header_frame, comment_header_frames = extract_ogg_header_frames(last_sample)
-                        if id_header_frame is not None and len(comment_header_frames) > 0:
-                            client.oggs_opus_header_frames += id_header_frame.raw_data
-                            for frame in comment_header_frames:
-                                client.oggs_opus_header_frames += frame.raw_data
-                            client.oggs_opus_header_frames_complete = True
-                        continue
-
-                # # Write to file for debugging.
-                # with open('/testing/sample.opus', 'wb') as f:
-                #     f.write(last_sample)
-
-                # Create temporary file for audio data which will be deleted after use.
-                with tempfile.NamedTemporaryFile(prefix='tmp_audio_', suffix='.wav', dir=settings['RAM_DISK_PATH'], delete=True) as temp_file:
-                    # Convert opus to wav
-                    opus_data = io.BytesIO(last_sample)
-                    opus_audio = AudioSegment.from_file(opus_data, format="ogg", frame_rate=48000, channels=2, sample_width=2)
-                    opus_audio.export(temp_file.name, format="wav")
-                    
-                    # Transcribe audio data
-                    result = audio_model.transcribe(temp_file.name, fp16=torch.cuda.is_available(), task=settings["TASK"])
-                    text = result['text'].strip()
-                    # logging.info(str.encode(text))
-
-                # Set transcription
-                with client.mutex:
-                    client.transcription = text
-
-                    # Send text to client
-                    try:
-                        client.send(str.encode(text))
-                    except:
-                        pass
-
-
-                # If enough time has passed between recordings, consider the phrase complete.
-                # Clear the current working audio buffer to start over with the new data.
-                if client.phrase_time and processing_start_time - client.phrase_time > timedelta(seconds=settings["RECORD_TIMEOUT"]):
-                    # logging.info("Clear audio buffer")
-                    client.clear_buffer()
-
-                processing_time.observe((datetime.utcnow() - processing_start_time).total_seconds())  # Observe processing time for prometheus
-                total_processing_time.observe((datetime.utcnow() - time_data_received).total_seconds())  # Observe total processing time for prometheus
-
-        # if KeyboardInterrupt stop. If everything else stop and show error.
-        except (KeyboardInterrupt, Exception) as e:
-            if isinstance(e, KeyboardInterrupt):
-                break   # Stop if Ctrl+C is pressed
-            else:
-                logging.error(e)
-                client.stop()
+    # Wait until stopped by Strg + C
+    try:
+        while True:
+            sleep(0.25)
+    except KeyboardInterrupt:
+        pass
 
 
     # Stop server
     STATUS = "stopping"
     logging.info("Stopping server...")
+    for w in workers:
+        w.stop()
     srv.stop()
     logging.info("Server stopped")
     STATUS = "stopped"
